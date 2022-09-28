d1d4df6f4ed54c95e2c32700188a515d4d5e8528
Joaomateus00 <joaoommateus@gmail.com>
2022-09-28 16:00:14 -0300

    Initial commit

---
+++ scripts/interface.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick)
+})
+
+})
+
+function handleClick(event){
+
+    
+    let square = event.target;
+    let position = square.id;
+
+    if(handleMove(position)){
+        
+        setTimeout(()=> {
+        alert ("O Jogo Acabou - O Vencedor foi " + playerTime)
+        },10)
+    }
+updateSquare(position);
+};
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbols = board [position];
+    square.innerHTML = `<div class='${symbols}'></div>`
+}
+
+function resetGame(){
+    
+    if (gameOver == true){
+        board = ['','','','','','','','',''];
+        playerTime = 0;
+        gameOver = false;
+    }
+    updateSquares(position);
+}
+function updateSquares(position){
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square)=> {
+        let position = square.id;
+        let symbols = board[position];
+        if (symbols != ''){
+        square.innerHTML = `<div class='${symbols}'></div>`
+        }
+})
+}
